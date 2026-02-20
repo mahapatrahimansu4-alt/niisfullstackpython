1a6ec6eb731aec7dc3818dc9a84989fd7f1e1162
himansu <mahapatrahimansu4@gmail.com>
2026-02-20 12:37:30 +0530

    hi program

---
+++ pythonniis/check9.py
@@ -0,0 +1,5 @@
+# sub two number without using two number
+a=10
+b=3
+c=a+ ~b+1 
+print(c)
---
+++ pythonniis/rectangle.py
@@ -0,0 +1,8 @@
+#wap take square side from keyboard display side area and perimeter.
+print("enter square side")
+s=int(input())
+ar=s*s 
+pr=4*s 
+print("side=",s)
+print("area=",ar)
+print("perimeter=",pr)
